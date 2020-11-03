cb486f10ed0ec8f0c27bed31117d0667f653d69a
omarion3698 <omaribinbakarivic@gmail.com>
2020-11-03 23:23:40 +0300

    Add api endpoints

--- sokoproject/settings.py
+++ sokoproject/settings.py
@@ -123,9 +123,9 @@ REST_FRAMEWORK = {
     ]
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
