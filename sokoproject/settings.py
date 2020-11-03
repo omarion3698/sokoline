9e028ba19b8d754046f4d04fc865385f0071ec06
AokoMercyline <aokomercyline34@gmail.com>
2020-11-03 21:56:08 +0300

    created users branch,files for user registration  and logout

--- sokoproject/settings.py
+++ sokoproject/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'soko',
     'users',
+    'rest_framework',
+    'knox',
+    
+    
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
