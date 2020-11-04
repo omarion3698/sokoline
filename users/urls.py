cf7159989b41d60c352a5d8fcc2129a6a6fe4944
omarion3698 <omaribinbakarivic@gmail.com>
2020-11-04 08:58:36 +0300

    Endpoints

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,6 @@ from knox import views as knox_views
 urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
     path('api/login/', LoginAPI.as_view(), name='login'),
-    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('api/user/', UserAPI.as_view(), name='user'),
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
